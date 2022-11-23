f1f6707d70e3fd3f11401ecb99714af087b2567c
victorBR116 <65189745+victorBR116@users.noreply.github.com>
2022-11-23 19:27:37 -0300

    att

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+function api(){
+    $url = "https://www.canalti.com.br/api/pokemons.json";
+     $pokemons = json_decode(file_get_contents($url));
+
+     foreach($pokemons->pokemon as $linha => $postagem){
+        var_dump(sprintf("<br>id:%s", $postagem->id)); 
+        var_dump(sprintf("<br>nome: %s", $postagem->name));
+        var_dump(sprintf('<br><img src="%s"/><hr>', $postagem->img));
+      
+
+      if($linha == 9){
+        exit();
+      }
+    }
+} 
+
+$resultado = api();
+return $resultado;
